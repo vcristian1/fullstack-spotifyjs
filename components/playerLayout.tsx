00d6beb6163a25121d7315cf9c7d61f2c453105b
Cristian Vargas <cristian.v0223@gmail.com>
2023-01-15 23:02:05 -0600

    initial commit

---
+++ components/playerLayout.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@chakra-ui/layout'
+// import Sidebar from './sidebar'
+// import PlayerBar from './playerBar'
+
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box>
+      <Box>Layout{children}</Box>
+    </Box>
+  )
+}
+
+export default PlayerLayout
