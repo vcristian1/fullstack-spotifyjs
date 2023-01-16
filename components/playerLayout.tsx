42a4962718f589b6e3c8715bc1146524f82d7be1
Cristian Vargas <cristian.v0223@gmail.com>
2023-01-16 10:23:02 -0600

    adjjusted margin bottom acorss the board in sidebar so all playlist can be seen and scrolled within its container

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -9,10 +9,10 @@ const PlayerLayout = ({ children }) => {
         <Sidebar />
       </Box>
       <Box marginLeft="250px" marginBottom="100px">
-        <Box height="calc(100vh - 100px)">Layout{children}</Box>
+        <Box height="calc(100vh - 100px)">{children}</Box>
       </Box>
       <Box position="absolute" left="0" bottom="0">
-        <PlayerBar /> 
+        <PlayerBar />
       </Box>
     </Box>
   )
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -50,6 +50,8 @@ const musicMenu = [
   },
 ]
 
+const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`)
+
 const Sidebar = () => {
   return (
     <Box
@@ -60,10 +62,10 @@ const Sidebar = () => {
       color="gray"
     >
       <Box paddingY="20px" height="100%">
-        <Box width="120px" marginBottom="20px" paddingX="20px">
+        <Box width="120px" marginBottom="10px" paddingX="20px">
           <NextImage src="/logo.svg" height={60} width={120} />
         </Box>
-        <Box marginBottom="20px">
+        <Box marginBottom="10px">
           <List spacing={2}>
             {navMenu.map((menu) => (
               <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
@@ -83,8 +85,7 @@ const Sidebar = () => {
             ))}
           </List>
         </Box>
-        <Divider color="gray.800" />
-        <Box marginTop="20px">
+        <Box marginBottom="10px" marginTop="20px">
           <List spacing={2}>
             {musicMenu.map((menu) => (
               <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
@@ -106,9 +107,17 @@ const Sidebar = () => {
         </Box>
         <Divider color="gray.800" />
         <Box height="66%" overflowY="auto" paddingY="20px">
-          {new Array(50).fill(1).map(() => (
-            <h1>Playlist</h1>
-          ))}
+          <List spaceing={2}>
+            {playlists.map((playlist) => (
+              <ListItem paddingX="20px" key={playlist}>
+                <LinkBox>
+                  <NextLink href="/" passHref>
+                    <LinkOverlay>{playlist}</LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
         </Box>
       </Box>
     </Box>
