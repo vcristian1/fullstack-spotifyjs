74e0273ba016485d902d84514e5e0066a487fc8b
Cristian Vargas <cristian.v0223@gmail.com>
2023-01-16 08:34:22 -0600

    created sidebar component and added basic styling to container to lay the groundwork for the rest of the application

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,12 +1,12 @@
 import { Box } from '@chakra-ui/layout'
-// import Sidebar from './sidebar'
+import Sidebar from './sidebar'
 // import PlayerBar from './playerBar'
 
 const PlayerLayout = ({ children }) => {
   return (
     <Box width="100vw" height="100vh">
       <Box position="absolute" top="0" width="250px" left="0">
-        {/* <Sidebar /> */}Sidebar
+        <Sidebar />
       </Box>
       <Box marginLeft="250px" marginBottom="100px">
         <Box height="calc(100vh - 100px)">Layout{children}</Box>
