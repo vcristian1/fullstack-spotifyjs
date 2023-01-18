823a2b36b5ea77d0f23b700192b2f553b780f4e0
Cristian Vargas <cristian.v0223@gmail.com>
2023-01-18 08:43:48 -0600

    added songs await functionin async run function which seeds songs in the database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -28,6 +28,7 @@ const run = async () => {
   // Logic to create a user, but first we encrpyt.
   const salt = bcrypt.genSaltSync()
   const user = await prisma.user.upsert({
+    // Queries, you need to have a unique property to query to use upsert
     where: { email: 'user@test.com' },
     update: {},
     create: {
@@ -35,6 +36,26 @@ const run = async () => {
       password: bcrypt.hashSync('password', salt),
     },
   })
+
+  const songs = await prisma.song.findMany({})
+  await Promise.all(
+    new Array(10).fill(1).map(async (_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist #${i + 1}`,
+          // We use the below syntax instead of userId: user.id to ensure prisma connects the user with this id to the user on this playlist
+          user: {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: songs.map((song) => ({
+              id: song.id,
+            })),
+          },
+        },
+      })
+    })
+  )
 }
 
 run()
