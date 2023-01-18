b3edabf16d4183fa77735654123c3e75862c3286
Cristian Vargas <cristian.v0223@gmail.com>
2023-01-17 21:12:45 -0600

    created seed.ts file and added run async function to seed the database, also added duration and url to song model in schema.prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,37 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcrypt'
+import { artistsData } from './songsData'
 
+const prisma = new PrismaClient()
+
+const run = async () => {
+  await Promise.all(
+    artistsData.map(async (artist) => {
+      // upsert means create or update, if it exists update it but if it doesnt exist create it.
+      return prisma.artist.upsert({
+        // Queries
+        where: { name: artist.name },
+        update: {},
+        create: {
+          name: artist.name,
+          songs: {
+            create: artist.songs.map((song) => ({
+              name: song.name,
+              duration: song.duration,
+              url: song.url,
+            })),
+          },
+        },
+      })
+    })
+  )
+}
+
+run()
+  .catch((e) => {
+    console.log(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
