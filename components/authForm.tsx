d50ac5c0c12fdd9cc311d4a195cac17cbd9eef2d
Cristian Vargas <cristian.v0223@gmail.com>
2023-01-19 17:28:25 -0600

    added logic in app.tsx so the sidebar is not seen on the signin or signup screen, also added the form in authForm with Next.js inputs and buttons

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -1,8 +1,8 @@
-import { Box, Flex, Input, Button } from "@chakra-ui/react";
+import { Box, Flex, Input, Button } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
-import { FC, useState } from "react";
+import { FC, useState } from 'react'
 // Stale While Revalidate from Vercel and Next.js: handles data fetching, caching, optimization updates, refetching and revalidating the cache for you.
-import { useSWRConfig } from "swr";
+import { useSWRConfig } from 'swr'
 import { auth } from '../lib/mutations'
 
 const AuthForm: FC<{ mode: string }> = ({ mode }) => {
@@ -12,16 +12,31 @@ const AuthForm: FC<{ mode: string }> = ({ mode }) => {
   const router = useRouter()
 
   return (
-    <Box height="100vh" width="100vw" bg="black">
-        <Flex justify="center" align="center" height="100px">
-            Hello
-        </Flex>
-        <Flex justify="center" align="center" height="calc(100vh - 100px)">
-            Form
-        </Flex>
+    <Box height="100vh" width="100vw" bg="black" color="white">
+      <Flex justify="center" align="center" height="100px">
+        Hello
+      </Flex>
+      <Flex justify="center" align="center" height="calc(100vh - 100px)">
+        <Box padding="50px" bg="gray.900" borderRadius="6px">
+          <form>
+            <Input
+              placeholder="email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" bg="green.500" isLoading={isLoading}>
+              {mode}
+            </Button>
+          </form>
+        </Box>
+      </Flex>
     </Box>
   )
-
 }
 
 export default AuthForm
